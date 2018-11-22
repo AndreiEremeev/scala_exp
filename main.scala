17fc95b644635c5d3635c753d3580a2471b4b8b5
Andrey Eremeev <Axtone4all@yandex.ru>
2018-11-23 00:56:47 +0300

    fixes

--- main.scala
+++ main.scala
@@ -9,54 +9,78 @@ case class Var(name: String) extends Term {
 }
 
 case class Appl(first: Term, second: Term) extends Term {
-  override def toString(): String = "("+first.toString()+" "+second.toString()+")"
-  override def isBelong(other: Var): Boolean = (first.isBelong(other) || second.isBelong(other))
+  override def toString(): String =
+    "("+first.toString()+" "+second.toString()+")"
+  override def isBelong(other: Var): Boolean =
+    (first.isBelong(other) || second.isBelong(other))
 }
 
 case class Abstr(variable: Var, body: Term) extends Term {
-  override def toString(): String = "λ"+variable.toString()+".("+body.toString()+")"
-  override def isBelong(other: Var): Boolean = body.isBelong(other)
+  override def toString(): String =
+    "λ"+variable.toString()+".("+body.toString()+")"
+  override def isBelong(other: Var): Boolean =
+    body.isBelong(other)
 }
 
 object Reducer {
-  def changeOfVariable(expression: Term, varOperand: Var, expressionOperand: Term): Term = expression match {
-    case Var(name) => if (name == varOperand.name ) expressionOperand else expression
-    case Appl(first, second) => new Appl(changeOfVariable(first, varOperand, expressionOperand), changeOfVariable(second, varOperand, expressionOperand))
-    case Abstr(variable, body) => 
-      if (variable.name == varOperand.name) {
-        expression
-      } else {
-        if (expressionOperand.isBelong(variable)) {
-          val newVar = new Var(variable.name+"`")
-          new Abstr(newVar, changeOfVariable(changeOfVariable(body, variable, newVar), varOperand, expressionOperand))
+  def changeOfVariable(expression: Term, varOperand: Var, expressionOperand: Term): Term =
+    expression match {
+      case Var(name) => {
+        if (name == varOperand.name)
+          expressionOperand
+        else
+          expression
+      }
+      case Appl(first, second) => {
+        new Appl(
+          changeOfVariable(first, varOperand, expressionOperand),
+          changeOfVariable(second, varOperand, expressionOperand)
+        )
+      }
+      case Abstr(variable, body) => {
+        if (variable.name == varOperand.name) {
+          expression
         } else {
-          new Abstr(variable, changeOfVariable(body, varOperand, expressionOperand))
+          if (expressionOperand.isBelong(variable)) {
+            val newVar = new Var(variable.name+"`")
+            new Abstr(
+              newVar,
+              changeOfVariable(
+                changeOfVariable(body, variable, newVar),
+                varOperand,
+                expressionOperand
+              )
+            )
+          } else {
+            new Abstr(variable, changeOfVariable(body, varOperand, expressionOperand))
+          }
         }
       }
   }
 
-  def alphaConversion(abstr : Abstr, variable : Var): Abstr = new Abstr(variable, changeOfVariable(abstr.body, abstr.variable, variable))
+  def alphaConversion(abstr : Abstr, variable : Var): Abstr =
+    new Abstr(
+      variable, 
+      changeOfVariable(abstr.body, abstr.variable, variable)
+    )
 
-  def changeOfVarString(expression: Term, varOperand: Var, expressionOperand: Term): String = {
-    "["+expressionOperand.toString()+"/"+varOperand.toString()+"]"+expression.toString()+" => "+changeOfVariable(expression, varOperand, expressionOperand)
-  }
-  def alphaConversionString(abstr : Abstr, variable : Var): String = abstr.toString()+" α => "+alphaConversion(abstr, variable).toString()
 }
-object HelloWorld {
+
+object Debug {
+  def alphaConversionString(abstr : Abstr, variable : Var): String = abstr.toString()+" α => "+Reducer.alphaConversion(abstr, variable).toString()
+  def changeOfVarString(expression: Term, varOperand: Var, expressionOperand: Term): String = "["+expressionOperand.toString()+"/"+varOperand.toString()+"]"+expression.toString() +" => "+Reducer.changeOfVariable(expression, varOperand, expressionOperand)
+  
+}
+
+object Main {
   def main(args: Array[String]) {
-    val tru = new Abstr(new Var("x"),new Abstr(new Var("y"), new Var("x")) )
-    println("TRUE == "+tru.toString())
-    val fls = new Abstr(new Var("x"),new Abstr(new Var("y"), new Var("y")) )
-    println("FALSE == "+fls.toString())
-    val simpleVar = new Var("x")
-    val simpleAppl = new Appl(new Var("x"), new Var("x"))
-    val simpleAbstr = new Abstr(new Var("x"), new Var("x"))
-    println(Reducer.changeOfVarString(simpleVar, new Var("x"), new Var("y")))
-    println(Reducer.changeOfVarString(simpleAbstr, new Var("x"), new Var("y")))
-    println(Reducer.changeOfVarString(simpleAppl, new Var("x"), new Var("y")))
-    println(Reducer.changeOfVarString(tru, new Var("x"), new Var("z")))
-    println(Reducer.changeOfVarString(tru, new Var("y"), new Var("z")))
-    println(Reducer.alphaConversionString(tru, new Var("z")))
-    println(Reducer.alphaConversionString(tru, new Var("y")))
+    val x = new Var("x")
+    val y = new Var("y")
+    val z = new Var("z")
+    val t = new Var("t")
+    val bigTerm = new Abstr(x,new Abstr(y, new Abstr(z, new Appl(x,new Appl(y,z)))))
+    println(bigTerm.toString())
+    println(Reducer.alphaConversion(bigTerm, t).toString())
+    println(Reducer.alphaConversion(bigTerm, z).toString())
   }
 }
